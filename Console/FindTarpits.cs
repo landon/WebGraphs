ea47b66e0aec76a799610263306857d17e70df65
landon <landon.rabern@gmail.com>
2015-04-12 22:18:02 -0400

    better output

--- Console/FindTarpits.cs
+++ Console/FindTarpits.cs
@@ -2,6 +2,7 @@
 using Choosability.WordGame;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,54 +11,72 @@ namespace Console
 {
     public static class FindTarpits
     {
-        const int Length = 4;
-        const bool RemovePermutationRedundancy = true;
+        const int Length = 5;
+        
+        static StreamWriter _sw;
 
         public static void Go()
         {
-            System.Console.ForegroundColor = ConsoleColor.White;
-
-            var tarpitEnumerator = new TarpitEnumerator(Length);
-            var count = 0;
-
-            var tarpitCores = new List<List<string>>();
-            foreach (var tarpit in tarpitEnumerator.EnumerateMinimalTarpits())
+            using(_sw =  new StreamWriter("tarpits" + Length + ".txt"))
             {
-                var isClosed = tarpitEnumerator.IsPermutationClosed(tarpit);
-                var core = tarpitEnumerator.RemovePermutationRedundancies(tarpit);
-
-                if (RemovePermutationRedundancy)
-                    System.Console.Write("{" + string.Join(",", core) + "}");
-                else
-                    System.Console.Write("{" + string.Join(",", tarpit) + "}");
-
-                if (!isClosed)
-                {
-                    System.Console.ForegroundColor = ConsoleColor.Red;
-                    System.Console.WriteLine(" (not closed)");
-                }
-                else
-                    System.Console.WriteLine();
-
                 System.Console.ForegroundColor = ConsoleColor.White;
-                System.Console.WriteLine();
-                count++;
 
-                tarpitCores.Add(core);
+                var tarpitEnumerator = new TarpitEnumerator(Length);
+                var count = 0;
+
+                var tarpitCores = new List<List<string>>();
+                foreach (var tarpit in tarpitEnumerator.EnumerateMinimalTarpits())
+                {
+                    var isClosed = tarpitEnumerator.IsPermutationClosed(tarpit);
+                    var core = tarpitEnumerator.RemovePermutationRedundancies(tarpit);
+
+                    Write("{" + string.Join(",", tarpit) + "}  :  ");    
+                    Write("{" + string.Join(",", core) + "}");
+
+                    if (!isClosed)
+                    {
+                        System.Console.ForegroundColor = ConsoleColor.Red;
+                        WriteLine(" (not closed)");
+                    }
+                    else
+                        WriteLine();
+
+                    System.Console.ForegroundColor = ConsoleColor.White;
+                    WriteLine();
+                    count++;
+
+                    tarpitCores.Add(core);
+                }
+
+                Write("found " + count + " tarpits");
+                WriteLine();
+                WriteLine("computing transversal hypergraph...");
+                WriteLine();
+
+                var H = new Hypergraph<string>(tarpitCores);
+                var transversals = H.Tr();
+
+                foreach (var t in transversals.E)
+                    WriteLine("{" + string.Join(",", t) + "}");
             }
+        }
 
-            System.Console.Write("found " + count + " tarpits");
+        static void Write(string s)
+        {
+            System.Console.Write(s);
+            _sw.Write(s);
+        }
+
+        static void WriteLine(string s)
+        {
+            System.Console.WriteLine(s);
+            _sw.WriteLine(s);
+        }
+
+        static void WriteLine()
+        {
             System.Console.WriteLine();
-            System.Console.WriteLine("computing transversal hypergraph...");
-            System.Console.WriteLine();
-
-            var H = new Hypergraph<string>(tarpitCores);
-            var transversals = H.Tr();
-
-            foreach (var t in transversals.E)
-            {
-                System.Console.WriteLine("{" + string.Join(",", t) + "}");
-            }
+            _sw.WriteLine();
         }
     }
 }
